51c352c5a22a0507da6df29af30dc94154e39cf2
1fishe2fishe <1fishe2fishe@duck.com>
2024-02-20 04:48:23 +0000

    Sound works without pipewire hack now

--- sound.c
+++ sound.c
@@ -14,27 +14,22 @@ double AiwniosGetVolume() {
 }
 static void AudioCB(void *ul, int8_t *out, int64_t len) {
   unsigned int i, i2;
-  int64_t fpb = len / have.channels;
+  int64_t fpb = len / have.channels / 2;
   for (i = 0; i < fpb; i++) {
     double t      = (double)++sample / have.freq;
     double amp    = -1.0 + 2.0 * round(fmod(2.0 * t * freq, 1.0));
-    int64_t maxed = (amp > 0) ? 127 : -127;
+    int64_t maxed = (amp > 0) ? 16384 : -16384;
     maxed *= vol;
     if (!freq)
       maxed = 0;
     for (i2 = 0; i2 != have.channels; i2++)
-      out[have.channels * i + i2] = maxed;
+      ((int16_t*)out)[have.channels * i + i2] = maxed;
   }
 }
 void SndFreq(int64_t f) {
   freq = f;
 }
 void InitSound() {
-#ifdef __linux__
-  SDL_SetHint(SDL_HINT_AUDIODRIVER, "pipewire");
-  if (SDL_Init(SDL_INIT_AUDIO))
-    SDL_SetHintWithPriority(SDL_HINT_AUDIODRIVER, NULL, SDL_HINT_OVERRIDE);
-#endif
   SDL_AudioSpec want;
   if (SDL_Init(SDL_INIT_AUDIO)) {
     // Audio failed to initailize
@@ -42,7 +37,7 @@ void InitSound() {
   }
   memset(&want, 0, sizeof(SDL_AudioSpec));
   want.freq     = 24000;
-  want.format   = AUDIO_S8;
+  want.format   = AUDIO_S16;
   want.channels = 2;
   want.samples  = 64;
   want.callback = (void *)&AudioCB;
