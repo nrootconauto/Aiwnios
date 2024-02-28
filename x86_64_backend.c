e9cac45fc2b7a15ee245158770631bb0466bdb13
nrootconauto <nrootconauto@gmail.com>
2024-02-28 15:09:10 -0500

    yh "reg" keywxrd wxrks I precolor the variables that are explictly defined.
    Scxrlx...,eh I mean Nrxxtcxnavto

--- x86_64_backend.c
+++ x86_64_backend.c
@@ -2728,15 +2728,15 @@ static int64_t GetSIBParts(CRPN *r, int64_t *off, CRPN **_b, CRPN **_idx,
   }
   //Adjust for offesets in base/index
   if(b)
-  while (__AddOffset(b, &tmp)) {
-	  b=__AddOffset(b, &tmp);
-	  i+=tmp;
-  }
+	  while (__AddOffset(b, &tmp)) {
+		  b=__AddOffset(b, &tmp);
+		  i+=tmp;
+	  }
   if(idx)
-  while (__AddOffset(idx, &tmp)) {
-	  idx=__AddOffset(idx, &tmp);
-	  i+=tmp*i2;
-  }
+	  while (__AddOffset(idx, &tmp)) {
+		  idx=__AddOffset(idx, &tmp);
+		  i+=tmp*i2;
+	  }
   if (_b)
     *_b = b;
   if (_idx)
