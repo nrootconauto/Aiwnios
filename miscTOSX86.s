0784a9cccf5d7eb7c62222a29fb6443d8f3d0970
1fishe2fishe <1fishe2fishe@duck.com>
2024-06-22 19:54:26 -0700

    format asm
    no change here, just a commit to not pollute diffs.

--- miscTOSX86.s
+++ miscTOSX86.s
@@ -1,69 +1,72 @@
+# -*- mode:intel-assembly; indent-tabs-mode:t; tab-width:8; coding:utf-8 -*-
+# vi: set noet ft=asm ts=8 sw=8 fenc=utf-8 :vi
 .intel_syntax noprefix
 
 .global TempleOS_CallN
 .global TempleOS_Call
 .global TempleOS_CallVaArgs
+
 # I64 Call(fptr)
 TempleOS_Call:
-  push rbp
-  mov  rbp,rsp
-  mov rax,qword ptr[rbp+16]
-  test rax,rax
-  jz .Lnull
-  call rax
-.Lnull:
-  pop rbp
-  ret 8
+	push	rbp
+	mov	rbp,rsp
+	mov	rax,qword ptr[rbp+16]
+	test	rax,rax
+	jz	0f
+	call	rax
+0:	pop	rbp
+	ret	8
+
 # I64 CallArgs(fptr,argc,argv)
 TempleOS_CallN:
-  push rbp
-  mov rbp,rsp
-  push rsi
-  push rdi
-  mov rcx,[rbp+3*8] #argc
-  mov rsi,[rbp+4*8] #argv 
-  shl rcx,3
-  sub rsp,rcx
-  mov rdi,rsp
-  rep movsb
-  call qword ptr [rbp+2*8] #fptr
-  pop rdi
-  pop rsi
-  pop rbp
-  ret 3*8
+	push	rbp
+	mov	rbp,rsp
+	push	rsi
+	push	rdi
+	mov	rcx,[rbp+3*8] #argc
+	mov	rsi,[rbp+4*8] #argv	
+	shl	rcx,3
+	sub	rsp,rcx
+	mov	rdi,rsp
+	rep	movsb
+	call	qword ptr[rbp+2*8] #fptr
+	pop	rdi
+	pop	rsi
+	pop	rbp
+	ret	3*8
+
 # I64 CallVaArgs(fptr,argc1,argv1,...)
 # fptr is a variadic fun with argc normal args
 # argc1/argv1 are propogated for fptr's varargs
 TempleOS_CallVaArgs:
-  push rbp
-  mov rbp,rsp
-  push rsi
-  push rdi
+	push	rbp
+	mov	rbp,rsp
+	push	rsi
+	push	rdi
+	mov	rcx,[rbp+3*8] #argc1
+	mov	rsi,[rbp+4*8] #argv1
+	shl	rcx,3
+	sub	rsp,rcx
+	mov	rdi,rsp
+	rep	movsb
+	push	qword ptr[rbp+3*8]
 
-  mov rcx,[rbp+3*8] #argc1
-  mov rsi,[rbp+4*8] #argv1
-  shl rcx,3
-  sub rsp,rcx
-  mov rdi,rsp
-  rep movsb
-  push qword ptr[rbp+3*8]
+	mov	rcx,[rbp+5*8] #argc
+	lea	rsi,[rbp+6*8] #argv	
+	lea	rcx,[8*rcx]
+	sub	rsp,rcx
+	mov	rdi,rsp
+	rep	movsb
 
-  mov rcx,[rbp+5*8] #argc
-  lea rsi,[rbp+6*8] #argv 
-  lea rcx,[8*rcx]
-  sub rsp,rcx
-  mov rdi,rsp
-  rep movsb
-
-  call qword ptr [rbp+2*8] #fptr
-  mov rcx,[rbp+3*8]#argc1
-  shl rcx,3
-  add rsp,rcx
-  mov rcx,[rbp+5*8]#argc
-  inc rcx#we pushed argc
-  shl rcx,3
-  add rsp,rcx
-  pop rdi
-  pop rsi
-  pop rbp
-  ret
+	call	qword ptr [rbp+2*8] #fptr
+	mov	rcx,[rbp+3*8]#argc1
+	shl	rcx,3
+	add	rsp,rcx
+	mov	rcx,[rbp+5*8]#argc
+	inc	rcx#we	pushed	argc
+	shl	rcx,3
+	add	rsp,rcx
+	pop	rdi
+	pop	rsi
+	pop	rbp
+	ret
